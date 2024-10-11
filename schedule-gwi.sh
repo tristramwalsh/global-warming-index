2ed7a9a58ea533c0d76edeb5e542fd79d786fe60
Tristram Walsh <tristramwalsh@gmail.com>
2024-10-11 13:13:12 +0000

    Organise slurm outputs into a dedicated directory.

--- schedule-gwi.sh
+++ schedule-gwi.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
-## Build the job index
+## Build the job index ########################################################
+
 # Create a sequential range for the range of regressed years.
 # This is for calculating the historical-only GWI:
 # array_values=`seq 2000 2023`  # This is inclusive of the start and end years
 # This is for calculating the GWI with all years:
-array_values=`seq 2000 2023`  # This sequence only includes 2023
+array_values=`seq 2020 2023`
 
 # Create array of subsampling sizes to calculate.
 # This is for scaling up the calculation:
 # array_samples=(60 65 70 75 80 85 90 95 100)  # Size of subsampling
 # This is for repeating final calculations at one size:
-array_samples=(90 90 90)  # Size of subsampling
+array_samples=(5 5)  # Size of subsampling
 
-### Generate a Slurm file for each Job ID
+### Generate a Slurm file for each Job ID #####################################
 
 WALLTIME=12:00:00
 SIM_NAME=gwi-hist
 SIM_CPUS=28
 SLURM_FILE_NAME=${SIM_NAME}_1850-
 LOG_DIR=slurm_logs
+mkdir -p ${LOG_DIR}
 
 # Keep track of which iteration we are on (avoid overwriting log files)
 count=1
@@ -46,22 +48,22 @@ cat > ${SLURM_FILE_NAME}${i}_${j}_${count}.slurm << EOF
 #SBATCH --job-name=${SIM_NAME}_1850-${i}_${j}_${count}
 
 ## Declare an output log for all jobs to use:
-#SBATCH --output=${SIM_NAME}_1850-${i}_${j}_${count}.out
+#SBATCH --output=./${LOG_DIR}/${SIM_NAME}_1850-${i}_${j}_${count}.out
 
 python gwi.py --samples=${j} --regress-range=1850-${i} --include-rate=n --include-headlines=n
-
 EOF
 
-# Submit a single job to the resource manager
+# Submit a single job to slurm.
 sbatch ${SLURM_FILE_NAME}${i}_${j}_${count}.slurm
 
-# Remove the job file as Slurm reads the script at submission time
+# Remove the job file as slurm reads the script at submission time and it is
+# no longer needed.
 rm -rf ${SLURM_FILE_NAME}${i}_${j}_${count}.slurm
 
 done
 
 # Increment the counter that keeps track of multiple runs at the same sample
-# size.
+# size. i.e. for each member of array_samples, this counter will increment.
 count=$((count + 1))
 
 done
